462dbca00433616c33a42ac16298e5ece0e8ff5f
Alija Hodzic <hodzic.alija5@gmail.com>
2022-03-29 17:30:49 +0200

    validations on model directly

--- models/list.model.js
+++ models/list.model.js
@@ -2,8 +2,8 @@ const { Schema, model, Types } = require("mongoose");
 
 const List = new Schema(
   {
-    title: { type: String, required: true, unique: true },
-    creator: { type: Types.ObjectId, required: true },
+    title: { type: String, required: "The list must have a title!", unique: true },
+    creator: { type: Types.ObjectId, required: "There must be a creator of a list!" },
     groceries: [{ product: String, amount: Number }],
   },
   { timestamps: true }
