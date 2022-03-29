97420b3b112f27b0bf3930286ea3d5244aaa0666
Alija Hodzic <hodzic.alija5@gmail.com>
2022-03-29 09:49:28 +0200

    jwt secret is in environmental variable

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const config = {
   port: process.env.PORT || 5000,
   mongo: process.env.MONGO_URI || `mongodb://localhost:27017`,
-  secret: process.env.JWT_SECRET || "bc0YSCBjmRzmxqqs7j3K",
+  secret: process.env.JWT_SECRET,
 };
 
 module.exports = config;
