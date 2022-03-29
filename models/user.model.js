dd42f119b0813a5befe145332c98bab60999b4f2
Alija Hodzic <hodzic.alija5@gmail.com>
2022-03-29 09:52:52 +0200

    hash and check password directly on model

--- models/user.model.js
+++ models/user.model.js
@@ -1,9 +1,23 @@
+const bcrypt = require("bcrypt");
 const { Schema, model } = require("mongoose");
 
-exports.User = model(
-  "User",
-  new Schema({
-    email: String,
-    password: String,
-  })
-);
+const User = new Schema({
+  email: String,
+  password: String,
+});
+
+User.methods.checkPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// hash password before user is saved
+User.pre("save", async function (next) {
+  if (!this.isModified) {
+    return next();
+  }
+
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+module.exports = model("User", User);
