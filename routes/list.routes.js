45d2952bef9afa6f2930dd680b9a57cb2c222f83
Alija Hodzic <hodzic.alija5@gmail.com>
2022-03-29 17:29:38 +0200

    list routes created and authenticated

--- routes/list.routes.js
+++ routes/list.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { create } = require("../controllers/list.controller");
+const { create, update, remove, getData } = require("../controllers/list.controller");
+const auth = require("../utils/helpers/auth");
 
-router.post("/", create);
+router.post("/", auth, create).patch("/", auth, update).delete("/", auth, remove).get("/", auth, getData);
 
 module.exports = router;
