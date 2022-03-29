0b369d93e19a792155d96d79c6901fec3645bf97
Alija Hodzic <hodzic.alija5@gmail.com>
2022-03-29 15:18:11 +0200

    error handled in change password controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -16,9 +16,9 @@ const register = async (req, res, next) => {
 
       return next(error);
     } else {
-      // in other case some fields are empty
+      // in other case some fields are empty so find costum message set on model
       let message = extractMessage(err);
-      let error = handleError(res, 401, message || "Something went horribly wrong!");
+      let error = handleError(res, message ? 401 : 500, message || "Something went horribly wrong!"); // if something goes horribly wrong we can't do much about it
 
       return next(error);
     }
@@ -59,13 +59,23 @@ const changePassword = async (req, res, next) => {
 
   try {
     await existingUser.save();
-  } catch (err) {
-    let error = handleError(res, 500, "Something went wrong!");
-    console.log(err);
-    return next(error);
-  }
 
-  res.status(200).json({ message: "Password changed successfuly!" });
+    res.status(200).json({ message: "Password changed successfuly!" });
+  } catch (err) {
+    // duplicate key error code.
+    if (err.code === 11000) {
+      // email must be unique so it caused this error
+      let error = handleError(res, 409, "User with that email already exists.");
+
+      return next(error);
+    } else {
+      // in other case some fields are empty so find costum message set on model
+      let message = extractMessage(err);
+      let error = handleError(res, message ? 401 : 500, message || "Something went horribly wrong!"); // if something goes horribly wrong we can't do much about it
+
+      return next(error);
+    }
+  }
 };
 
 module.exports = { register, login, changePassword };
